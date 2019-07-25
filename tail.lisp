6f6ebfa4c111a535b7d1ff4a5495e9b1fd50a2ab
aleneus <aleneus@gmail.com>
2019-07-25 11:58:51 +0300

    add some hot keys

--- tail.lisp
+++ tail.lisp
@@ -36,6 +36,16 @@
 (global-set-key (kbd "C-'") 'comment-region)
 (global-set-key (kbd "C-M-'") 'uncomment-region)
 
+;; switching between windows
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
+
+;; bookmarks
+(global-set-key [f5] 'bookmark-set)
+(global-set-key [f6] 'bookmark-jump)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; python
 (defun my-python-hook ()
