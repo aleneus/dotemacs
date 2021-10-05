d2ccff2416547c86c9431b838b631ace48773b74
Aleksandr Popov <aleneus@gmail.com>
2021-10-05 21:23:20 +0300

    elpy: setup venv

--- init.el
+++ init.el
@@ -156,6 +156,7 @@
 (defun my-python-hook ()
   (require 'flycheck)
   (require 'elpy)
+  (require 'jedi)
 
   (my-common-prog)
 
@@ -167,11 +168,14 @@
   (setq indent-tabs-mode nil)
 
   (setq elpy-rpc-python-command "python3")
+  (setq elpy-rpc-backend "jedi")
+  (setq elpy-rpc-virtualenv-path 'current)
 
   (setq flycheck-checker 'python-pylint)
 
   (elpy-enable)
   (elpy-mode)
+
   (outline-minor-mode)
   (flycheck-mode)
 
