402ed853c56be6ef4207c0c131d185c2cb07241c
Aleksandr Popov <aleneus@gmail.com>
2022-12-23 11:32:24 +0300

    md: spell check and writegood

--- emacs.d/modes/markdown.el
+++ emacs.d/modes/markdown.el
@@ -1,4 +1,10 @@
+(require 'flyspell)
+(require 'flyspell-popup)
+(require 'writegood-mode)
+
 (defun my-markdown-mode-hook ()
-  (visual-line-mode))
+  (visual-line-mode)
+  (flyspell-mode)
+  (writegood-mode))
 
 (add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
