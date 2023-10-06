ea157af40a991a5611df90c2cf3a1f29e3b0ac5e
Aleksandr Popov <aleneus@gmail.com>
2023-10-06 13:22:58 +0300

    brush init

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,13 +2,15 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; Settings that do not require the installation of 
+;; Settings that do not require the installation of
 ;; additional packages
 (load "~/.emacs.d/modules/gui.el")
 (load "~/.emacs.d/modules/backup.el")
 (load "~/.emacs.d/modules/editing.el")
 (load "~/.emacs.d/modules/ffap.el")
 
+
+;; Other settings with additional packages
 (load "~/.emacs.d/modules/packages.el")
 
 (load "~/.emacs.d/modules/scrolling.el")
@@ -16,7 +18,10 @@
 (load "~/.emacs.d/modules/popwin.el")
 (load "~/.emacs.d/modules/navigation.el")
 
+
+;; Modes for writing code
 (load "~/.emacs.d/modules/common-prog.el")
+
 (load "~/.emacs.d/modules/c-cpp.el")
 (load "~/.emacs.d/modules/go.el")
 (load "~/.emacs.d/modules/python.el")
