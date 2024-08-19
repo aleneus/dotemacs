0aafa73455b0367461d747acf3811c1a533b4c7d
Aleksandr Popov <aleneus@gmail.com>
2024-08-19 09:54:38 +0300

    roam: + tag and db sync keys

--- emacs.d/modules/roam.el
+++ emacs.d/modules/roam.el
@@ -4,7 +4,9 @@
   (org-roam-directory (file-truename "~/org-roam"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c d s" . org-roam-db-sync)
+         ("C-c t a" . org-roam-tag-add))
   :config
   (org-roam-setup))
 
