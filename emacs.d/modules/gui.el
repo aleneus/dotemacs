20545ebe7fe01a58236180132b64743ebd9bc121
Aleksandr Popov <aleneus@gmail.com>
2024-12-12 15:40:03 +0300

    auto revert

--- emacs.d/modules/gui.el
+++ emacs.d/modules/gui.el
@@ -7,3 +7,5 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
+
+(global-auto-revert-mode t)
