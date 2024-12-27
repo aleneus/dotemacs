3a3f1d12ebebae0b63bda7e1fcd6d01132d10bfb
Aleksandr Popov <aleneus@gmail.com>
2024-12-27 11:27:56 +0300

    python-lsp: yapfify: handy

--- emacs.d/modules/python-lsp.el
+++ emacs.d/modules/python-lsp.el
@@ -16,15 +16,13 @@
   (:map python-mode-map
         ("C-c d" . lsp-find-definition)
         ("C-c r" . lsp-find-references)
-        ("C-c n" . lsp-rename))
+        ("C-c n" . lsp-rename)
+        ("C-c y" . yapfify-buffer))
 
   :hook
   ((python-mode . common-prog)
    (python-mode . lsp)
-   (python-mode . flycheck-mode)
-   (python-mode . (lambda ()
-                    (add-hook 'before-save-hook
-                              'yapfify-buffer)))))
+   (python-mode . flycheck-mode)))
 
 (use-package lsp-ui
   :commands lsp-ui-mode)
