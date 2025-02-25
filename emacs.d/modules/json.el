d6a551966e36b608e3edd2be9a058d73bb3ae74f
Aleksandr Popov <aleneus@bk.ru>
2025-02-25 09:29:46 +0300

    json-mode: + require, + tab

--- emacs.d/modules/json.el
+++ emacs.d/modules/json.el
@@ -1,8 +1,10 @@
 ;; sudo apt install jsonlint
+(require 'json-mode)
 
 (defun my-json-mode-hook ()
   (flycheck-mode)
   (hs-minor-mode)
-  (setq json-mode-indent-level 4))
+  (setq json-mode-indent-level 4)
+  (global-set-key (kbd "<C-tab>") 'hs-toggle-hiding))
 
 (add-hook 'json-mode-hook 'my-json-mode-hook)
