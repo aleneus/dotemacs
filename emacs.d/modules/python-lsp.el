a8ee990b9e2840dbc89c4c7ebc35904f4822a249
Aleksandr Popov <aleneus@bk.ru>
2025-07-03 15:18:40 +0300

    python: company on

--- emacs.d/modules/python-lsp.el
+++ emacs.d/modules/python-lsp.el
@@ -1,31 +1,6 @@
 ;; pip install 'python-lsp-server[all]'
 ;; pip install pylint
 
-(use-package python-mode
-  :config
-  (setq tab-width 4)
-  (setq indent-tabs-mode nil)
-  (setq flycheck-checker 'python-pylint)
-
-  :bind
-  (:map python-mode-map
-		("C-c d" . xref-find-definitions)
-		("C-c r" . xref-find-references)
-		("C-c n" . eglot-rename))
-
-  :hook
-  (python-mode . (lambda ()
-				   (eglot-ensure)
-				   (flycheck-mode)
-				   (add-hook 'before-save-hook 'yapfify-buffer nil t)
-				   (common-prog))))
-
-(use-package eglot
-  :ensure t
-  :config
-  (add-to-list 'eglot-server-programs
-			   '(python-mode . ("pylsp"))))
-
 (use-package flycheck
   :ensure t)
 
@@ -33,4 +8,30 @@
   :ensure t)
 
 (use-package python-mode
-  :ensure t)
+  :ensure t
+
+  :config
+  (setq tab-width 4)
+  (setq indent-tabs-mode nil)
+  (setq flycheck-checker 'python-pylint)
+
+  :hook
+  (python-mode . (lambda ()
+				   (eglot-ensure)
+				   (flycheck-mode)
+				   (add-hook 'before-save-hook 'yapfify-buffer nil t)
+				   (common-prog)
+				   (define-key python-mode-map (kbd "C-c d") 'xref-find-definitions)
+				   (define-key python-mode-map (kbd "C-c r") 'xref-find-references)
+				   (define-key python-mode-map (kbd "C-c n") 'eglot-rename)
+				   (setq comment-start "# "))))
+
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs
+			   '(python-mode . ("pylsp"))))
+
+(use-package company
+  :ensure t
+  :hook (python-mode . company-mode))
