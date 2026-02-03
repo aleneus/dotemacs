7dd70725530a1491de4581317bdc153be60efd1f
Aleksandr Popov <aleneus@bk.ru>
2026-02-03 11:21:15 +0300

    helm: fix tab behaviour

--- emacs.d/modules/helm.el
+++ emacs.d/modules/helm.el
@@ -2,9 +2,14 @@
 
 (use-package helm
   :ensure t
+  :init
+  (setq helm-find-files-sort-directories t)
   :bind (
 		 ("M-x" . helm-M-x)
 		 ("C-x C-f" . helm-find-files)
 		 ("C-x b" . helm-buffers-list))
   :config
-  (helm-mode 1))
+  (helm-mode 1)
+  (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-z") 'helm-select-action)
+  (define-key helm-find-files-map (kbd "<tab>") 'helm-execute-persistent-action))
